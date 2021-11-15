e7b2d0da5211597d9f6a5d7dce55df92e37464e1
Vardges Musakhanyan <danoville@gmail.com>
2021-11-15 13:58:41 +0400

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Polymorphism.Shape;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Circle circle = new Circle();
+            circle.Radius = radius;
+            Console.WriteLine("Area :" + circle.Area());
+
+
+            double length = Convert.ToDouble(Console.ReadLine());
+            Square square = new Square();
+            square.Length = length;
+            Console.WriteLine("Area :" + square.Area());
+
+            double height = Convert.ToDouble(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle();
+            rectangle.Height = height;
+            rectangle.Width = width;
+            Console.WriteLine("Area :" + rectangle.Area());
+
+
+
+            Shape circleShape = new Circle(radius);
+            Console.WriteLine("Area :" + circleShape.Area());
+
+            Shape squareShape = new Square(length);
+            Console.WriteLine("Area :" + squareShape.Area());
+
+            Shape rectangleShape = new Rectangle(height, width);
+            Console.WriteLine("Area :" + rectangleShape.Area());
+
+        }
+    }
+}
---
+++ Polymorphism/Shape.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Shape
+    {
+        public virtual double Area()
+        {
+            return default;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Square : Shape
+    {
+        public double Length { get; set; }
+
+        public Square()
+        {
+
+        }
+
+        public Square(double length)
+        {
+            Length = length;
+        }
+
+        public override double Area()
+        {
+            return Math.Pow(Length, 2);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(double height, double  width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Height * Width;
+        }
+    }
+}
